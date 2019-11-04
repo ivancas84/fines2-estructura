7d787bc485e2fae068515758444616d25d49007a
ivan <ivancas84@gmail.com>
2019-11-04 11:40:49 -0300

    agregar scripts y archivos adicionales

---
+++ script/crear_comision_456.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once("../config/config.php");
+require_once("class/model/Dba.php");
+require_once("class/model/Values.php");
+require_once("class/model/Render.php");
+
+$comision = EntityValues::getInstanceRequire("comision",DEFAULT_VALUE);
+echo "<pre>";
+print_r($comision);
+
+/*
+$render = new Render();
+$render->setCondition([
+  ["com_dvi_serie","=","C_191"]
+]);
+$rows = Dba::all("curso",$render);
+
+echo "<table>";
+foreach($rows as $row){
+  echo "<tr>";
+  echo "<td>".$row["id"]."</td>";
+  echo "<td>".$row["toma_activa"]."</td>";
+  echo "<td>".$row["ta_pro_nombres"]."</td>";
+  echo "<td>".$row["ta_pro_apellidos"]."</td>";
+  echo "<td>".$row["ta_observaciones"]."</td>";
+  echo "</tr>";
+}
+echo "</table>";
+*/
---
+++ script/sedes_456.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once("../config/config.php");
+require_once("class/model/Dba.php");
+require_once("class/model/Render.php");
+require_once("function/array_unique_key.php");
+require_once("function/array_group_value.php");
+
+$render = new Render();
+$render->setCondition([
+  ["numero","=", true]
+]);
+$render->setOrder(["numero"=>"ASC"]);
+$sedes =Dba::all("sede",$render);
+$idSedes = array_unique_key($sedes, "id");
+
+
+$render = new Render();
+$render->setCondition([
+  ["sed_id","=", $idSedes]
+]);
+$referentes =Dba::all("referente",$render);
+$sedesReferentes = array_group_value($referentes, "sede");
+
+  echo "<table>";
+  foreach($sedes as $sede){
+    $r = EntitySqlo::getInstanceRequire("sede")->values($sede);
+    echo "<tr>";
+    echo "<td>".$r["sede"]->numero()."</td>";
+    echo "<td>".$r["sede"]->nombre()."</td>";
+    echo "<td>".$r["domicilio"]->label()."</td>";
+    echo "<td>";
+    if(array_key_exists($r["sede"]->id(), $sedesReferentes)){
+      echo "<table>";
+      foreach($sedesReferentes[$r["sede"]->id()] as $referente){
+        $re = EntitySqlo::getInstanceRequire("referente")->values($referente);
+        echo "<tr><td>".$re["persona"]->nombre()."</td>";
+        echo "<td>".$re["persona"]->telefonos()."</td></tr>";
+      }
+      echo "</table>";
+    }
+    echo"</td>";
+    echo "</tr>";
+  }
+  echo "</table>";
