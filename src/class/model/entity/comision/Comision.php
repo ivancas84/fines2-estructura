6faa29351602fde9a96020f675910259bb7561ba
ivan <ivancas84@gmail.com>
2019-12-13 12:31:52 -0300

    definir campos unicos multiples para comision: sede division y fecha anio

--- src/class/model/entity/comision/Comision.php
+++ src/class/model/entity/comision/Comision.php
@@ -3,5 +3,11 @@
 require_once("class/model/entity/comision/Main.php");
 
 class ComisionEntity extends ComisionEntityMain {
-
+    public function getFieldsUniqueMultiple(){
+        return array(
+          Field::getInstanceRequire("comision", "division"),
+          Field::getInstanceRequire("comision", "fecha_anio"),
+          Field::getInstanceRequire("comision", "sede"),
+        );
+      }
 }
