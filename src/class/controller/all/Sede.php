64e9649e891863aae71bfef0805a424670b1f4fa
unknown <ivancas84@gmail.com>
2020-01-02 06:29:38 -0300

    implementar display render de sede

--- src/class/controller/all/Sede.php
+++ src/class/controller/all/Sede.php
@@ -5,34 +5,5 @@ require_once("class/controller/All.php");
 class SedeAll extends All {
   public $entityName = "sede";
   
-  protected function renderComisionCondition($key){
-    if($this->display["params"][$key]) {
-      unset($this->display["params"][$key]);
-      $this->renderComision->addCondition($key, "=", $this->display["params"][$key]);
-    }
-  }
-
-  public function main($display) {
-    $this->display = $display;
-    if(array_key_exists ("params" , $this->display)  && 
-      (array_key_exists ("modalidad" , $this->display["params"]) 
-      || array_key_exists ("fecha_anio" , $this->display["params"]) 
-      || array_key_exists ("fecha_semestre" , $this->display["params"])
-    )) {
-      $this->renderComision = new Render();
-      $this->renderComisionCondition("modalidad");
-      $this->renderComisionCondition("fecha_anio");
-      $this->renderComisionCondition("fecha_semestre");
-
-      $comisiones = Ma::all("comision", $render);
-      $sedes = array_unique_key($comisiones, "sede");
-    }
-
-    $render = Render::getInstanceDisplay($display);
-    if(isset($sedes))  $render->addCondition("id","=",$sedes);
-    
-    return $render;
-  }
-
 }
 
--- src/class/controller/displayRender/Sede.php
+++ src/class/controller/displayRender/Sede.php
@@ -1,8 +1,58 @@
 <?php
 
 require_once("class/controller/DisplayRender.php");
+require_once("function/array_unique_key.php");
 
 class SedeDisplayRender extends DisplayRender {
   public $entityName = "sede";
+
+  protected function renderComisionCondition($key){
+    if(array_key_exists ($key , $this->display["params"])
+    && (!empty($this->display["params"][$key]))) {
+        $this->renderComision->addCondition($key, "=", $this->display["params"][$key]);
+        $this->paramsComision = true;
+    }
+  }
+  
+  public function main($display) {
+    $this->display = $display;
+    $this->paramsComision = false;
+    if(array_key_exists ("params" , $this->display)){
+      $this->renderComision = new Render();
+      foreach($this->display["params"] as $key => $value){
+        switch($key) {
+          case "centro_educativo":
+            /** 
+             * parametros de sede 
+             */
+            continue;
+          break;
+          case "modalidad":
+            /**
+             * parametros de comision
+             */
+            $this->renderComisionCondition($key);
+            unset($this->display["params"][$key]);
+          break;
+          
+          default:
+            /**
+             * parametros no validos
+             */
+            unset($this->display["params"][$key]);
+        }
+      }
+      if($this->paramsComision){
+        $comisiones = Ma::all("comision", $this->renderComision);
+        $sedes = array_unique_key($comisiones, "sede");
+      }
+    }
+
+    $render = RenderAux::getInstanceDisplay($this->display);
+    if(isset($sedes)) $render->addCondition("id","=",$sedes);
+
+    return $render;
+  }
+
 }
 
