132b89231b54cd9a4c3d403bb1a82e75c5b2f36a
unknown <ivancas84@gmail.com>
2020-02-03 08:10:25 -0300

    nuevo campo horario

--- src/class/model/entity/curso/Curso.php
+++ src/class/model/entity/curso/Curso.php
@@ -3,5 +3,10 @@
 require_once("class/model/entity/curso/Main.php");
 
 class CursoEntity extends CursoEntityMain {
+  public function getFieldsNf(){
+    $array = parent::getFieldsNf();
+    array_push($array, Field::getInstanceRequire("curso","horario"));
+    return $array;
+  }
 
 }
