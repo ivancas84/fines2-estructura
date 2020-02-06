8ea62cbacd3d4d4ba9e4d10b9d5ef99ba0d2d339
ivan <ivancas84@gmail.com>
2020-02-06 15:21:59 -0300

    actualizar core

--- src/class/controller/all/Comision.php
+++ src/class/controller/all/Comision.php
@@ -1,8 +1,14 @@
 <?php
 
 require_once("class/controller/All.php");
+require_once("class/model/Ma.php");
+require_once("class/model/Render.php");
+require_once("class/tools/Filter.php");
+require_once("class/controller/DisplayRender.php");
+
 
 class ComisionAll extends All {
   public $entityName = "comision";
+
 }
 
