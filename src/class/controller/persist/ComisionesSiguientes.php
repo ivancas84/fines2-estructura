ab3ff3d07e21766e2d4d7f04fae8af525e5e87d8
ivan <ivancas84@gmail.com>
2020-02-17 12:23:30 -0300

    incorporar metodos de ModelTools e instanciar Render a traves de nuevo metodo getInstanceParams

--- src/class/controller/persist/ComisionesSiguientes.php
+++ src/class/controller/persist/ComisionesSiguientes.php
@@ -14,48 +14,8 @@ class ComisionesSiguientesPersist extends Persist {
    */
   protected $entityName = "comisiones_siguientes";
 
-
-  public function consultarComisiones(array $p){
-    /**
-     * Si existe al menos una comision para los parametros indicados no se realiza la consulta
-     */
-    $render = [
-      ["fecha_anio", "=", $p["fecha_anio"]],
-      ["fecha_semestre", "=", $p["fecha_semestre"]],
-      ["modalidad", "=", $p["modalidad"]]
-    ];
-
-    return Ma::all("comision",$render);
-  }
-
-  public function definirParametros($data){
-    /**
-     * Define los parametros correctos de la consulta de comisiones anteriores a la solicitada
-     */
-    $param["fecha_anio"] = intval($data["fecha_anio"]);
-    $param["fecha_semestre"] = intval($data["fecha_semestre"]);
-    $param["modalidad"] = $data["modalidad"];
-    
-    switch($param["fecha_semestre"]){
-      case 2:  
-        $param["fecha_semestre"] = 1; 
-      break;
-      case 1: 
-        $param["fecha_anio"]--;
-        $param["fecha_semestre"] = 2; 
-      break;
-      
-      default: 
-        $param["fecha_semestre"] = false;
-        $param["fecha_anio"]--;
-    }
-
-    return $param;
-  }
-
   public function main($data){
-
-    /**
+     /**
      * @param $data["fecha_anio"] //fecha anio a calcular
      * @param $data["fecha_semestre"] //fecha semestre a calcular
      * @param $data["modalidad"] //modalidad a calcular
@@ -63,10 +23,14 @@ class ComisionesSiguientesPersist extends Persist {
     if(empty($data["fecha_anio"])) throw new Exception("Dato no definido: fecha anio");
     if(empty($data["fecha_semestre"])) throw new Exception("Dato no definido: fecha semestre");
     if(empty($data["modalidad"])) throw new Exception("Dato no definido: modalidad");
-
-    if(count($this->consultarComisiones($data))) throw new Exception("Ya existen comisiones para los parametros ingresados");
-    $param = $this->definirParametros($data);
-    $comisionesAnteriores = $this->consultarComisiones($param);
+    if(empty($data["sed_centro_educativo"])) throw new Exception("Dato no definido: centro educativo");
+   
+    $render = Render::getInstanceParams($data);
+    if(Ma::count("comision",$render)) throw new Exception("Ya existen comisiones para los parametros ingresados");
+    
+    $dataAnterior = ModelTools::intervaloAnterior($data);
+    $render = Render::getInstanceParams($dataAnterior);
+    $comisionesAnteriores = Ma::all("comision",$render);
     if(!count($comisionesAnteriores)) throw new Exception("No existen comisiones anteriores para tomar de referencia");
   
     foreach($comisionesAnteriores as $comision){
