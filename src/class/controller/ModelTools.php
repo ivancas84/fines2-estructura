36b08e671de1c430e2898175fedd26758bdee946
ivan <ivancas84@gmail.com>
2020-02-17 12:22:54 -0300

    limpiar metodos de ModelTools

--- src/class/controller/ModelTools.php
+++ src/class/controller/ModelTools.php
@@ -2,14 +2,31 @@
 
 class ModelTools {
 
-  public static function grupoAnterior($grupo){
+  public static function intervaloAnterior(array $grupo){
     /**
+     * un intervalo es la combinación de fecha_anio, fecha_semestre
      * @param $grupo["fecha_anio"]
      * @param $grupo["fecha_semestre"]
-     * @param $grupo["modalidad"]
-     * @param $grupo["centro_educativo"]
      */
+    $param = $grupo;
 
-     if
+    $param["fecha_anio"] = intval($grupo["fecha_anio"]);
+    $param["fecha_semestre"] = intval($grupo["fecha_semestre"]);
+
+    switch($param["fecha_semestre"]){
+      case 2:  
+        $param["fecha_semestre"] = 1; 
+      break;
+      case 1: 
+        $param["fecha_anio"]--;
+        $param["fecha_semestre"] = 2; 
+      break;
+      
+      default: 
+        $param["fecha_semestre"] = false;
+        $param["fecha_anio"]--;
+    }
+
+    return $param;
   }
 }
