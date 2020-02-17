74510909acdad4c6af51ff352c70ed9fa84e851d
unknown <ivancas84@gmail.com>
2020-02-17 07:49:05 -0300

    incorporacion de ModelTools

---
+++ src/class/controller/ModelTools.php
@@ -0,0 +1,15 @@
+<?php
+
+class ModelTools {
+
+  public static function grupoAnterior($grupo){
+    /**
+     * @param $grupo["fecha_anio"]
+     * @param $grupo["fecha_semestre"]
+     * @param $grupo["modalidad"]
+     * @param $grupo["centro_educativo"]
+     */
+
+     if
+  }
+}
---
+++ src/class/controller/persist/HorariosCursosGrupo.php
@@ -0,0 +1,97 @@
+<?php
+
+
+require_once("class/controller/Persist.php");
+require_once("class/model/Ma.php");
+require_once("class/model/Values.php");
+require_once("class/controller/persist/ComisionCursos.php");
+
+
+
+class HorariosCursosPersist extends Persist {
+  /**
+   * Persistencia de cursos y comisiones
+   */
+  protected $entityName = "horarios_cursos";
+
+
+  public function consultarComisiones(array $p){
+    /**
+     * Si existe al menos una comision para los parametros indicados no se realiza la consulta
+     */
+    $render = [
+      ["fecha_anio", "=", $p["fecha_anio"]],
+      ["fecha_semestre", "=", $p["fecha_semestre"]],
+      ["modalidad", "=", $p["modalidad"]]
+    ];
+
+    return Ma::all("comision",$render);
+  }
+
+  public function definirParametros($data){
+    /**
+     * Define los parametros correctos de la consulta de comisiones anteriores a la solicitada
+     */
+    $param["fecha_anio"] = intval($data["fecha_anio"]);
+    $param["fecha_semestre"] = intval($data["fecha_semestre"]);
+    $param["modalidad"] = $data["modalidad"];
+    
+    switch($param["fecha_semestre"]){
+      case 2:  
+        $param["fecha_semestre"] = 1; 
+      break;
+      case 1: 
+        $param["fecha_anio"]--;
+        $param["fecha_semestre"] = 2; 
+      break;
+      
+      default: 
+        $param["fecha_semestre"] = false;
+        $param["fecha_anio"]--;
+    }
+
+    return $param;
+  }
+
+  public function main($data){
+
+    /**
+     * @param $data["fecha_anio"] //fecha anio a calcular
+     * @param $data["fecha_semestre"] //fecha semestre a calcular
+     * @param $data["modalidad"] //modalidad a calcular
+     * 
+     */
+    if(empty($data["fecha_anio"])) throw new Exception("Dato no definido: fecha anio");
+    if(empty($data["fecha_semestre"])) throw new Exception("Dato no definido: fecha semestre");
+    if(empty($data["modalidad"])) throw new Exception("Dato no definido: modalidad");
+    if(empty($data["centro_educativo"])) throw new Exception("Dato no definido: centro_educativo");
+
+    //if(!count($this->consultarCursos($data))) throw new Exception("Ya existen comisiones para los parametros ingresados");
+    //$param = $this->definirParametros($data);
+    $cursos = $this->consultarCursos($data);
+    if(!count($cursos)) throw new Exception("No existen cursos");
+  
+    foreach($cursos as $curso){
+      /*
+      $nuevaComision = EntityValues::getInstanceRequire("comision");
+      $nuevaComision->_fromArray($comision);
+      $nuevaComision->setId(null);
+      $nuevaComision->setFechaAnio($data["fecha_anio"]);
+      $nuevaComision->setFechaSemestre($data["fecha_semestre"]);
+      $nuevaComision->resetTramoSiguiente();
+      if($nuevaComision->_logs()->isError()) continue;
+
+      $controller = new ComisionCursosPersist();
+      $controller->main($nuevaComision->_toArray());
+      array_push($this->logs, ["sql"=>$controller->getSql(), "detail"=>$controller->getDetail()]);
+      */
+    }
+  
+  }
+
+
+}
+
+
+
+
