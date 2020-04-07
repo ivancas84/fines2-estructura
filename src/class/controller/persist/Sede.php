48715153d46d8012af9b0c538dc6c8e6ea532fd3
unknown <ivancas84@gmail.com>
2020-04-07 08:03:43 -0300

    Desarrollo de nuevo controlador de persistencia de sede

--- src/class/controller/persist/Sede.php
+++ src/class/controller/persist/Sede.php
@@ -7,39 +7,36 @@ class SedePersist extends Persist {
     protected $entityName = "sede";
 
     public function main($data){
-        print_r($data);
-        /*foreach($data as $d) {
-            switch($d["entity"]) {
-                case "domicilio": $domicilio = $d["row"]; break;
-                case "sede": $sede = $d["row"]; break;
-            }                    
-        }
+
+        if(!array_key_exists($data["sede"])) throw new Exception ("Sede no definida");
         
-        if($sede["id"]) {
-            $ce_bd = Ma::getOrNull("sede", $sede["id"]);
+        //Si una sede no tiene el domicilio definido, no estara en el array $data
+        $existeDomicilio = (array_key_exists($data["domicilio"])) ? true : false;
+
+        if($data["sede"]["id"]) {
+            $sede_bd = Ma::getOrNull("sede", $data["sede"]["id"]);
         } else {
-            $ce_bd = Ma::oneOrNull("sede", [
-                ["numero","=",$sede["numero"]],
-                ["centro_educativo","=",$sede["centro_educativo"]],
+            $sede_bd = Ma::oneOrNull("sede", [
+                ["numero","=",$data["sede"]["numero"]],
+                ["centro_educativo","=",$data["sede"]["centro_educativo"]],
             ]);    
         }
         
         $deleteDomicilio = false;
-        if (!empty($ce_bd)){
-            $sede["id"] = $ce_bd["id"];
-            if(isset($domicilio)) {
-                if (!empty($ce_bd["domicilio"])) $domicilio["id"] = $ce_bd["domicilio"];    
+        if (!empty($sede_bd)){
+            $data["sede"]["id"] = $sede_bd["id"];
+            if(isset($domiclilio)) {
+                if (!empty($sede_bd["domicilio"])) $domicilio["id"] = $sede_bd["domicilio"];    
             } else {
-                if (!empty($ce_bd["domicilio"])) $deleteDomicilio = $ce_bd["domicilio"];
-                $sede["domicilio"] = null;
+                if (!empty($sede_bd["domicilio"])) $deleteDomicilio = $sede_bd["domicilio"];
+                $data["sede"]["domicilio"] = null;
             }
         }
 
-        if(isset($domicilio)) $sede["domicilio"] = $this->row("domicilio", $domicilio);
+        if(isset($domicilio)) $data["sede"]["domicilio"] = $this->row("domicilio", $domicilio);
 
-
-        $this->row("sede", $sede);
-        if($deleteDomicilio) $this->delete("domicilio", $deleteDomicilio);*/
+        $this->row("sede", $data["sede"]);
+        if($deleteDomicilio) $this->delete("domicilio", $deleteDomicilio);
     }
 
 }
