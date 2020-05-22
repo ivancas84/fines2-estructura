fca2af65a34aef936d64fed3cb2ffab62434f01f
ivancas84 <ivancas84@gmail.com>
2020-05-22 15:37:34 -0300

    modificar documentacion, cambio el valor por defecto de los dates

--- doc/migracion_db/2017.2-2020.2.2/1 telefono.sql
+++ doc/migracion_db/2017.2-2020.2.2/1 telefono.sql
@@ -1,8 +1,11 @@
 SELECT CONCAT(
-    "INSERT INTO planfi10_20202.email (id, email, persona) VALUES ('", 
-    id, "', ", 
-	IF(email IS NOT NULL, CONCAT("'", email, "'"), 'null'), ", ", 
-	IF(id IS NOT NULL, CONCAT("'", id, "'"), 'null'), 
+    "INSERT INTO planfi10_20202.telefono (id, tipo, prefijo, numero, insertado, eliminado, persona) VALUES ('", 
+    HEX(id), "', ", 
+	IF(tipo IS NOT NULL, CONCAT("'", tipo, "'"), 'null'), ", ",
+    IF(prefijo IS NOT NULL, CONCAT("'", prefijo, "'"), 'null'), ", ",
+    IF(numero IS NOT NULL, CONCAT("'", numero, "'"), 'null'), ", ",
+    IF(alta IS NOT NULL, CONCAT("'", alta, "'"), 'null'), ", ",
+    IF(baja IS NOT NULL, CONCAT("'", baja, "'"), 'null'), ", ", 
+	IF(persona IS NOT NULL, CONCAT("'", HEX(persona), "'"), 'null'), 
     "); "
-) FROM persona
-WHERE email IS NOT NULL;
+) FROM telefono;
