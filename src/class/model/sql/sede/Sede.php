f05032b9e1c6ab6af493a8f4c4bfc856a54ead61
ivancas84 <ivancas84@gmail.com>
2020-05-27 10:02:50 -0300

    Actualizar sede

--- src/class/model/sql/sede/Sede.php
+++ src/class/model/sql/sede/Sede.php
@@ -4,6 +4,25 @@ require_once("class/model/sql/sede/Main.php");
 
 class SedeSql extends SedeSqlMain {
 
+  public function _mappingField($field){
+    $p = $this->prf();
+    $t = $this->prt();
+    if($f = parent::_mappingField($field)) return $f;
+    switch ($field) {
+      case $p.'numero_trim': return "TRIM(LEADING 0 FROM {$t}.numero)";
+      default: return null;
+    }
+  }
+
+  public function _conditionFieldStruct($field, $option, $value){
+    $p = $this->prf();
+    $f = $this->_mappingField($field);
+    switch ($field) {
+      case $p.'numero_trim': return $this->format->conditionText($f, $value, $option);
+      default: return parent::_conditionFieldStruct($field, $option, $value);
+    }
+  }
+
   public function _subSql(Render $render) {
     $t = $this->prt();
     return "(
