300e3a2086463afed821de4d2e1887f6acc7329a
ivancas84 <ivancas84@gmail.com>
2020-05-28 19:57:44 -0300

    reorganizar archivos de base de datos

--- doc/create.sql
+++ doc/create.sql
@@ -1,7 +1,8 @@
-CREATE SCHEMA IF NOT EXISTS `fines2_2020` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci ;
-USE fines2_2020;
+CREATE SCHEMA IF NOT EXISTS `planfi10_20203` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci ;
+USE planfi10_20203;
 
-CREATE TABLE IF NOT EXISTS `domicilio` (
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`domicilio` (
   `id` VARCHAR(45) NOT NULL,
   `calle` VARCHAR(45) NOT NULL,
   `entre` VARCHAR(45) NULL DEFAULT NULL,
@@ -15,7 +16,7 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `centro_educativo` (
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`centro_educativo` (
   `id` VARCHAR(45) NOT NULL,
   `nombre` VARCHAR(255) NOT NULL,
   `cue` VARCHAR(45) NULL DEFAULT NULL,
@@ -25,14 +26,14 @@ CREATE TABLE IF NOT EXISTS `centro_educativo` (
   UNIQUE INDEX `cue_UNIQUE` (`cue` ASC),
   CONSTRAINT `fk_centro_educativo_domicilio1`
     FOREIGN KEY (`domicilio`)
-    REFERENCES `domicilio` (`id`)
+    REFERENCES `planfi10_20203`.`domicilio` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `tipo_sede` (
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`tipo_sede` (
   `id` VARCHAR(45) NOT NULL,
   `descripcion` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
@@ -41,7 +42,7 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `sede` (
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`sede` (
   `id` VARCHAR(45) NOT NULL,
   `numero` VARCHAR(45) NOT NULL,
   `nombre` VARCHAR(255) NOT NULL,
@@ -57,31 +58,32 @@ CREATE TABLE IF NOT EXISTS `sede` (
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_sede_domicilio1`
     FOREIGN KEY (`domicilio`)
-    REFERENCES `domicilio` (`id`)
+    REFERENCES `planfi10_20203`.`domicilio` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_sede_tipo_sede1`
     FOREIGN KEY (`tipo_sede`)
-    REFERENCES `tipo_sede` (`id`)
+    REFERENCES `planfi10_20203`.`tipo_sede` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_sede_centro_educativo1`
     FOREIGN KEY (`centro_educativo`)
-    REFERENCES `centro_educativo` (`id`)
+    REFERENCES `planfi10_20203`.`centro_educativo` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `persona` (
+
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`persona` (
   `id` VARCHAR(45) NOT NULL,
   `nombres` VARCHAR(255) NOT NULL,
   `apellidos` VARCHAR(255) NULL DEFAULT NULL,
   `fecha_nacimiento` DATE NULL DEFAULT NULL,
   `numero_documento` VARCHAR(45) NOT NULL,
   `cuil` VARCHAR(45) NULL DEFAULT NULL,
-  `email` VARCHAR(255) NULL DEFAULT NULL,
   `genero` VARCHAR(45) NULL DEFAULT NULL,
   `apodo` VARCHAR(255) NULL DEFAULT NULL,
   `alta` TIMESTAMP NOT NULL DEFAULT current_timestamp,
@@ -92,14 +94,15 @@ CREATE TABLE IF NOT EXISTS `persona` (
   INDEX `fk_persona_domicilio1_idx` (`domicilio` ASC),
   CONSTRAINT `fk_persona_domicilio1`
     FOREIGN KEY (`domicilio`)
-    REFERENCES `domicilio` (`id`)
+    REFERENCES `planfi10_20203`.`domicilio` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `cargo` (
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`cargo` (
   `id` VARCHAR(45) NOT NULL,
   `descripcion` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
@@ -108,7 +111,9 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `designacion` (
+
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`designacion` (
   `id` VARCHAR(45) NOT NULL,
   `desde` DATE NULL DEFAULT NULL,
   `hasta` DATE NULL DEFAULT NULL,
@@ -122,24 +127,25 @@ CREATE TABLE IF NOT EXISTS `designacion` (
   INDEX `fk_designacion_persona1_idx` (`persona` ASC),
   CONSTRAINT `fk_designacion_cargo1`
     FOREIGN KEY (`cargo`)
-    REFERENCES `cargo` (`id`)
+    REFERENCES `planfi10_20203`.`cargo` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_designacion_sede1`
     FOREIGN KEY (`sede`)
-    REFERENCES `sede` (`id`)
+    REFERENCES `planfi10_20203`.`sede` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_designacion_persona1`
     FOREIGN KEY (`persona`)
-    REFERENCES `persona` (`id`)
+    REFERENCES `planfi10_20203`.`persona` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `plan` (
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`plan` (
   `id` VARCHAR(45) NOT NULL,
   `orientacion` VARCHAR(45) NOT NULL,
   `resolucion` VARCHAR(45) NULL DEFAULT NULL,
@@ -149,7 +155,7 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `asignatura` (
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`asignatura` (
   `id` VARCHAR(45) NOT NULL,
   `nombre` VARCHAR(255) NOT NULL,
   `formacion` VARCHAR(45) NULL DEFAULT NULL,
@@ -162,7 +168,8 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `modalidad` (
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`modalidad` (
   `id` VARCHAR(45) NOT NULL,
   `nombre` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
@@ -171,47 +178,28 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `comision` (
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`calendario` (
   `id` VARCHAR(45) NOT NULL,
-  `turno` VARCHAR(45) NULL DEFAULT NULL,
-  `division` VARCHAR(45) NOT NULL,
-  `anio` VARCHAR(45) NULL DEFAULT NULL,
-  `semestre` VARCHAR(45) NULL DEFAULT NULL,
-  `comentario` TEXT NULL DEFAULT NULL,
-  `autorizada` TINYINT(1) NOT NULL,
-  `apertura` TINYINT(1) NOT NULL,
-  `publicada` TINYINT(1) NOT NULL,
-  `fecha_anio` YEAR NULL DEFAULT NULL,
-  `fecha_semestre` INT(11) NULL DEFAULT NULL,
-  `observaciones` TEXT NULL DEFAULT NULL,
-  `alta` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `sede` VARCHAR(45) NOT NULL,
+  `inicio` DATE NOT NULL,
+  `fin` DATE NOT NULL,
+  `anio` YEAR NOT NULL,
+  `semestre` SMALLINT(6) NOT NULL,
+  `insertado` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_spanish_ci;
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`planificacion` (
+  `id` VARCHAR(45) NOT NULL,
+  `anio` VARCHAR(45) NOT NULL,
+  `semestre` VARCHAR(45) NOT NULL,
   `plan` VARCHAR(45) NOT NULL,
-  `modalidad` VARCHAR(45) NOT NULL,
-  `comision_siguiente` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_comision_sede1_idx` (`sede` ASC),
-  INDEX `fk_comision_comision1_idx` (`comision_siguiente` ASC),
-  INDEX `fk_comision_plan1_idx` (`plan` ASC),
-  INDEX `fk_comision_modalidad1_idx` (`modalidad` ASC),
-  CONSTRAINT `fk_comision_sede1`
-    FOREIGN KEY (`sede`)
-    REFERENCES `sede` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_comision_comision1`
-    FOREIGN KEY (`comision_siguiente`)
-    REFERENCES `comision` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_comision_plan1`
+  INDEX `fk_planificacion_plan1_idx` (`plan` ASC),
+  CONSTRAINT `fk_planificacion_plan1`
     FOREIGN KEY (`plan`)
-    REFERENCES `plan` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_comision_modalidad1`
-    FOREIGN KEY (`modalidad`)
-    REFERENCES `modalidad` (`id`)
+    REFERENCES `planfi10_20203`.`plan` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -219,7 +207,62 @@ DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
 
-CREATE TABLE IF NOT EXISTS `curso` (
+
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`comision` (
+  `id` VARCHAR(45) NOT NULL,
+  `turno` VARCHAR(45) NULL DEFAULT NULL,
+  `division` VARCHAR(45) NOT NULL,
+  `comentario` TEXT NULL DEFAULT NULL,
+  `autorizada` TINYINT(1) NOT NULL,
+  `apertura` TINYINT(1) NOT NULL,
+  `publicada` TINYINT(1) NOT NULL,
+  `observaciones` TEXT NULL DEFAULT NULL,
+  `alta` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `sede` VARCHAR(45) NOT NULL,
+  `modalidad` VARCHAR(45) NOT NULL,
+  `planificacion` VARCHAR(45) NOT NULL,
+  `comision_siguiente` VARCHAR(45) NULL DEFAULT NULL,
+  `calendario` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_comision_sede1_idx` (`sede` ASC),
+  INDEX `fk_comision_comision1_idx` (`comision_siguiente` ASC),
+  INDEX `fk_comision_modalidad1_idx` (`modalidad` ASC),
+  INDEX `fk_comision_calendario1_idx` (`calendario` ASC),
+  INDEX `fk_comision_planificacion1_idx` (`planificacion` ASC),
+  CONSTRAINT `fk_comision_sede1`
+    FOREIGN KEY (`sede`)
+    REFERENCES `planfi10_20203`.`sede` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comision_comision1`
+    FOREIGN KEY (`comision_siguiente`)
+    REFERENCES `planfi10_20203`.`comision` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comision_modalidad1`
+    FOREIGN KEY (`modalidad`)
+    REFERENCES `planfi10_20203`.`modalidad` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comision_calendario1`
+    FOREIGN KEY (`calendario`)
+    REFERENCES `planfi10_20203`.`calendario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comision_planificacion1`
+    FOREIGN KEY (`planificacion`)
+    REFERENCES `planfi10_20203`.`planificacion` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_spanish_ci;
+
+
+
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`curso` (
   `id` VARCHAR(45) NOT NULL,
   `horas_catedra` INT(11) NOT NULL,
   `comision` VARCHAR(45) NOT NULL,
@@ -230,19 +273,20 @@ CREATE TABLE IF NOT EXISTS `curso` (
   INDEX `fk_curso_asignatura1_idx` (`asignatura` ASC),
   CONSTRAINT `fk_curso_comision1`
     FOREIGN KEY (`comision`)
-    REFERENCES `comision` (`id`)
+    REFERENCES `planfi10_20203`.`comision` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_curso_asignatura1`
     FOREIGN KEY (`asignatura`)
-    REFERENCES `asignatura` (`id`)
+    REFERENCES `planfi10_20203`.`asignatura` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `dia` (
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`dia` (
   `id` VARCHAR(45) NOT NULL,
   `numero` SMALLINT(1) NOT NULL,
   `dia` VARCHAR(9) NOT NULL,
@@ -253,7 +297,8 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `horario` (
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`horario` (
   `id` VARCHAR(45) NOT NULL,
   `hora_inicio` TIME NOT NULL,
   `hora_fin` TIME NOT NULL,
@@ -264,25 +309,21 @@ CREATE TABLE IF NOT EXISTS `horario` (
   INDEX `fk_horario_dia1_idx` (`dia` ASC),
   CONSTRAINT `fk_horario_curso1`
     FOREIGN KEY (`curso`)
-    REFERENCES `curso` (`id`)
+    REFERENCES `planfi10_20203`.`curso` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_horario_dia1`
     FOREIGN KEY (`dia`)
-    REFERENCES `dia` (`id`)
+    REFERENCES `planfi10_20203`.`dia` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `toma` (
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`toma` (
   `id` VARCHAR(45) NOT NULL,
   `fecha_toma` DATE NULL DEFAULT NULL,
-  `fecha_inicio` DATE NULL DEFAULT NULL,
-  `fecha_fin` DATE NULL DEFAULT NULL,
-  `fecha_contralor` DATE NULL DEFAULT NULL,
-  `fecha_consejo` DATE NULL DEFAULT NULL,
   `estado` VARCHAR(45) NULL DEFAULT NULL,
   `observaciones` TEXT NULL DEFAULT NULL,
   `comentario` VARCHAR(45) NULL DEFAULT NULL,
@@ -298,44 +339,112 @@ CREATE TABLE IF NOT EXISTS `toma` (
   INDEX `fk_toma_persona2_idx` (`reemplazo` ASC),
   CONSTRAINT `fk_toma_curso1`
     FOREIGN KEY (`curso`)
-    REFERENCES `curso` (`id`)
+    REFERENCES `planfi10_20203`.`curso` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_toma_persona1`
     FOREIGN KEY (`docente`)
-    REFERENCES `persona` (`id`)
+    REFERENCES `planfi10_20203`.`persona` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_toma_persona2`
     FOREIGN KEY (`reemplazo`)
-    REFERENCES `persona` (`id`)
+    REFERENCES `planfi10_20203`.`persona` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `distribucion_horaria` (
+
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`distribucion_horaria` (
   `id` VARCHAR(45) NOT NULL,
   `horas_catedra` INT(11) NOT NULL,
   `dia` INT(11) NOT NULL,
-  `anio` VARCHAR(45) NOT NULL,
-  `semestre` VARCHAR(45) NOT NULL,
-  `plan` VARCHAR(45) NOT NULL,
   `asignatura` VARCHAR(45) NOT NULL,
+  `planificacion` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_distribucion_horaria_plan1_idx` (`plan` ASC),
   INDEX `fk_distribucion_horaria_asignatura1_idx` (`asignatura` ASC),
-  CONSTRAINT `fk_distribucion_horaria_plan1`
-    FOREIGN KEY (`plan`)
-    REFERENCES `plan` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  INDEX `fk_distribucion_horaria_planificacion1_idx` (`planificacion` ASC),
   CONSTRAINT `fk_distribucion_horaria_asignatura1`
     FOREIGN KEY (`asignatura`)
-    REFERENCES `asignatura` (`id`)
+    REFERENCES `planfi10_20203`.`asignatura` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_distribucion_horaria_planificacion1`
+    FOREIGN KEY (`planificacion`)
+    REFERENCES `planfi10_20203`.`planificacion` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`email` (
+  `id` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `verificado` TINYINT(1) NOT NULL DEFAULT 0,
+  `insertado` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `eliminado` TIMESTAMP NULL DEFAULT NULL,
+  `persona` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_email_persona1_idx` (`persona` ASC),
+  CONSTRAINT `fk_email_persona1`
+    FOREIGN KEY (`persona`)
+    REFERENCES `planfi10_20203`.`persona` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_spanish_ci;
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`telefono` (
+  `id` VARCHAR(45) NOT NULL,
+  `tipo` VARCHAR(45) NULL DEFAULT NULL,
+  `prefijo` VARCHAR(45) NULL DEFAULT NULL,
+  `numero` VARCHAR(255) NOT NULL,
+  `insertado` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `eliminado` TIMESTAMP NULL DEFAULT NULL,
+  `persona` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_telefono_persona1_idx` (`persona` ASC),
+  CONSTRAINT `fk_telefono_persona1`
+    FOREIGN KEY (`persona`)
+    REFERENCES `planfi10_20203`.`persona` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_spanish_ci;
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`file` (
+  `id` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `type` VARCHAR(255) NOT NULL,
+  `content` VARCHAR(255) NOT NULL,
+  `size` INT(10) UNSIGNED NOT NULL,
+  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_spanish_ci;
+
+CREATE TABLE IF NOT EXISTS `planfi10_20203`.`contralor` (
+  `id` VARCHAR(45) NOT NULL,
+  `fecha_contralor` DATE NULL DEFAULT NULL,
+  `fecha_consejo` DATE NULL DEFAULT NULL,
+  `observaciones` TEXT NULL DEFAULT NULL,
+  `insertado` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `toma` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_contralor_toma1_idx` (`toma` ASC),
+  CONSTRAINT `fk_contralor_toma1`
+    FOREIGN KEY (`toma`)
+    REFERENCES `planfi10_20203`.`toma` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_spanish_ci;
+
