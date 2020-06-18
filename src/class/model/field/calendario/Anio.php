a92a8cdf1ec02ddd4c622a8c4644a4be27c56eb8
ivancas84 <ivancas84@gmail.com>
2020-06-18 17:04:40 -0300

    incluir anio como campo principal

--- src/class/model/field/calendario/Anio.php
+++ src/class/model/field/calendario/Anio.php
@@ -3,6 +3,8 @@
 require_once("class/model/field/calendario/_Anio.php");
 
 class FieldCalendarioAnio extends _FieldCalendarioAnio {
+  public $main = true;
+  
   public function __construct() {
     parent::__construct();
     $this->minLength = 2000;
