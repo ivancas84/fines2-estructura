a32097a512b685e1548bfec7c199fb5fba0c1f0f
ivancas84 <ivancas84@gmail.com>
2020-09-03 00:38:11 -0300

    Agregar controlador para persistir registro docente

---
+++ src/class/api/persist/RegistroDocente.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once("class/api/Persist.php");
+
+class RegistroDocentePersistApi extends PersistApi {
+
+  public function main(){
+    $data = Filter::jsonPostRequired();
+
+    if(empty($data)) throw new Exception("Se está intentando persistir un conjunto de datos vacío");
+
+    $values = $this->container->getValues("persona")->_fromArray($data)->_reset();
+    if(!$values->_check()) throw new Exception($values->_getLogs()->toString());
+    
+    $detail = [];
+    /**
+     * Solo se registrara el detalle de las actualizaciones
+     * El cache debe actualizarse solo con las actualizaciones
+     * Las inserciones no se encuentran cacheadas y seran posteriormente consultadas
+     */
+    
+    $row = $this->container->getDb()->unique("persona", $values->_toArray());
+    if($row) throw new Exception("Ya se encuentra registrado");
+
+    $values->_setDefault();
+    $persist = $this->container->getSqlo("persona")->insert($values->_toArray());
+    $this->container->getDb()->multi_query_transaction_log($persist["sql"]);
+    
+    return ["id" => $persist["id"], "detail" => $detail];
+  }
+}
