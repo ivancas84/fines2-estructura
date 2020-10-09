0acd69d502cc5979fdfeb82b10e8b677c640a4bc
ivancas84 <ivancas84@gmail.com>
2020-10-09 11:44:23 -0300

    Corregir error en el nombre de la clase

--- src/class/model/entity/Persona.php
+++ src/class/model/entity/Persona.php
@@ -2,7 +2,7 @@
 
 require_once("class/model/entity/_Persona.php");
 
-class Persona extends _Persona  {
+class PersonaEntity extends _PersonaEntity  {
  
   public $main = ["nombres","apellidos","numero_documento"];
   
