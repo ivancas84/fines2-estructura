1f70e7247b14717fff5e81ab389307cfd1851985
ivancas84 <ivancas84@gmail.com>
2020-12-06 19:08:03 -0300

    Incorporar nuevos campos derivados exclusivos

--- src/class/model/entity/Persona.php
+++ src/class/model/entity/Persona.php
@@ -5,5 +5,8 @@ require_once("class/model/entity/_Persona.php");
 class PersonaEntity extends _PersonaEntity  {
  
   public $main = ["nombres","apellidos","numero_documento"];
-  
+  public $nf = ['nombres', 'apellidos', 'fecha_nacimiento', 'numero_documento', 'cuil', 'genero', 'apodo', 'telefono', 'email', 'email_abc', 'alta',
+    'primer_nombre_apellido', 'primer_apellido_nombre'];
+  public $noAdmin = ['primer_nombre_apellido', 'primer_apellido_nombre'];
+
 }
