a038a8c266263ed84edb1f2732edd5d6c193e9ad
ivancas84 <ivancas84@gmail.com>
2020-12-15 01:32:03 -0300

    Nueva clase para definir metodos mapping de Comision

---
+++ src/class/model/mapping/Comision.php
@@ -0,0 +1,27 @@
+<?php
+require_once("class/model/entityOptions/Mapping.php");
+
+class ComisionMapping extends MappingEntityOptions{
+
+  public function numero() {
+    return "CONCAT({$this->_pf()}sed.numero, {$this->_pt()}.division)
+";
+  }
+
+
+  public function tramo() {
+    return "CONCAT(COALESCE({$this->_pf()}pla.anio,''), COALESCE({$this->_pf()}pla.semestre,''))";
+  }
+
+  public function label() {
+    return "CONCAT(
+      {$this->_pf()}sed.numero, {$this->_pt()}.division,
+      IF({$this->_pf()}pla.id, CONCAT({$this->_pf()}pla.anio,{$this->_pf()}pla.semestre), ''),
+      IF({$this->_pf()}cal.id, CONCAT(' ',{$this->_pf()}cal.anio,'-',{$this->_pf()}cal.semestre), ''),
+      IF({$this->_pf()}cal.inicio,CONCAT(' ', {$this->_pf()}cal.inicio),''),
+      IF({$this->_pf()}cal.fin,CONCAT(' ', {$this->_pf()}cal.fin),'')
+    )";
+  }
+
+
+}
