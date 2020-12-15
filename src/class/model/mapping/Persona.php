5238bd3eaf376f79971250a1b09faf7797775216
ivancas84 <ivancas84@gmail.com>
2020-12-15 01:31:25 -0300

    correcciones minimas

--- src/class/model/mapping/Persona.php
+++ src/class/model/mapping/Persona.php
@@ -9,7 +9,7 @@ class PersonaMapping extends MappingEntityOptions{
   }
 
   public function primerApellidoNombre() {
-    return "CONCAT_WS(' ', SUBSTRING_INDEX( {$this->_pt()}.nombres , ' ', 1 ), SUBSTRING_INDEX( {$this->_pt()}.apellidos , ' ', 1 )      
+    return "CONCAT_WS(' ', SUBSTRING_INDEX( {$this->_pt()}.apellidos , ' ', 1 ), SUBSTRING_INDEX( {$this->_pt()}.nombres , ' ', 1 )      
     )";
   }
 
