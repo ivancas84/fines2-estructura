77a99c131b22b0080bba042c3e97a6d7bbd5d264
unknown <ivancas84@gmail.com>
2021-02-04 00:34:22 -0300

    Indicar campo unico

--- src/class/model/entity/PlanillaDocente.php
+++ src/class/model/entity/PlanillaDocente.php
@@ -5,5 +5,6 @@ require_once("class/model/entity/_PlanillaDocente.php");
 class PlanillaDocenteEntity extends _PlanillaDocenteEntity {
 
   public $main = ["numero"];
+  public $unique = ["numero"];
 
 }
