4c6059368b39cabc4ca5ef463b0b0a3783e770d5
unknown <ivancas84@gmail.com>
2021-02-09 13:16:22 -0300

    Eliminar campos de la tabla alumno

--- src/class/model/entity/_Alumno.php
+++ src/class/model/entity/_Alumno.php
@@ -6,7 +6,7 @@ require_once("class/model/Field.php");
 class _AlumnoEntity extends Entity {
   public $name = "alumno";
   public $alias = "alum";
-  public $nf = ['fotocopia_documento', 'partida_nacimiento', 'creado', 'constancia_cuil', 'certificado_estudios', 'anio_ingreso', 'activo', 'observaciones'];
+  public $nf = ['fotocopia_documento', 'partida_nacimiento', 'creado', 'constancia_cuil', 'certificado_estudios', 'anio_ingreso', 'activo', 'programa', 'observaciones'];
   public $mu = ['persona', 'comision'];
   public $_u = [];
   public $notNull = ['id', 'fotocopia_documento', 'partida_nacimiento', 'creado', 'constancia_cuil', 'certificado_estudios', 'persona'];
