973ec15cbab72c1091739fde69cad92f1e628529
ivancas84 <ivancas84@gmail.com>
2021-02-17 13:43:58 -0300

    Correcciones minimas

---
+++ src/class/model/value/Comision.php
@@ -0,0 +1,44 @@
+<?php
+require_once("class/model/entityOptions/Value.php");
+require_once("function/nombres_parecidos.php");
+
+class ComisionValue extends ValueEntityOptions{
+
+  public function planificacionSiguiente(){
+    
+
+    $planificacion = $this->container->getDb()->get("planificacion",$this->_get("planificacion"));
+    if (
+      (empty($planificacion["anio"])) 
+      || (empty($planificacion["semestre"]))
+      || (empty($planificacion["plan"]))
+    ) return UNDEFINED;
+
+    $anio = intval($planificacion["anio"]);
+    $semestre = intval($planificacion["semestre"]);
+    
+    if(($anio == 3) && ($semestre == 2)) return null;
+
+    $render = new Render();
+    $render->setCondition([
+      ["plan","=",$planificacion["plan"]],
+    ]);
+
+    if($semestre == 1) {
+      $render->addCondition([
+        ["anio","=",$anio],
+        ["semestre","=",++$semestre],
+      ]);
+    } else {
+      $render->addCondition([
+        ["anio","=",++$anio],
+        ["semestre","=",$semestre],
+      ]);
+    }
+
+    $planificacion = $this->container->getDb()->oneOrNull("planificacion",$render);
+
+    return ($planificacion) ? $planificacion["id"] : null;
+  }
+
+}
