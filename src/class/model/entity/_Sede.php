0dee91b299d422c36920457e0bec77488f86ee02
ivancas84 <ivancas84@gmail.com>
2021-02-25 21:08:40 -0300

    Incorporar campo fecha traspaso

--- src/class/model/entity/_Sede.php
+++ src/class/model/entity/_Sede.php
@@ -6,7 +6,7 @@ require_once("class/model/Field.php");
 class _SedeEntity extends Entity {
   public $name = "sede";
   public $alias = "sede";
-  public $nf = ['numero', 'nombre', 'observaciones', 'alta', 'baja'];
+  public $nf = ['numero', 'nombre', 'observaciones', 'alta', 'baja', 'fecha_traspaso'];
   public $mu = ['domicilio', 'tipo_sede', 'centro_educativo'];
   public $_u = [];
   public $notNull = ['id', 'numero', 'nombre', 'alta'];
