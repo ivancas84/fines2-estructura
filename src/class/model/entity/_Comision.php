64937ce5257d1a6a6673ef35084252e56e7d2202
ivancas84 <ivancas84@gmail.com>
2021-02-28 21:49:44 -0300

    Incorporar campos necesarios para definir informes a la Direccion de Adultos

--- src/class/model/entity/_Comision.php
+++ src/class/model/entity/_Comision.php
@@ -6,7 +6,7 @@ require_once("class/model/Field.php");
 class _ComisionEntity extends Entity {
   public $name = "comision";
   public $alias = "comi";
-  public $nf = ['turno', 'division', 'comentario', 'autorizada', 'apertura', 'publicada', 'observaciones', 'alta', 'identificacion'];
+  public $nf = ['turno', 'division', 'comentario', 'autorizada', 'apertura', 'publicada', 'observaciones', 'alta', 'identificacion', 'estado', 'configuracion'];
   public $mu = ['sede', 'modalidad', 'planificacion', 'comision_siguiente', 'calendario'];
   public $_u = [];
   public $notNull = ['id', 'division', 'autorizada', 'apertura', 'publicada', 'alta', 'sede', 'modalidad', 'calendario'];
