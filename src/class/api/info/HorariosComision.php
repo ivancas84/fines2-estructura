1480b6094b6e58f20bae04cba791b27e5ef35cf2
ivancas84 <ivancas84@gmail.com>
2021-03-12 10:38:10 -0300

    Correcciones minimas

--- src/class/api/info/HorariosComision.php
+++ src/class/api/info/HorariosComision.php
@@ -2,13 +2,15 @@
 
 require_once("class/api/Base.php");
 
+require_once("function/php_input.php");
 
 
 class HorariosComisionInfoApi extends BaseApi {
   public $entityName = "horarios_comision";
 
-  public function main($idComisiones) {
-    return ModelTools::diasHorariosComision($idComisiones);
+  public function main() {
+    $ids = php_input();
+    return $this->container->getController("model_tools")->diasHorariosComision($ids);
   }
 
 }
