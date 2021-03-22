5ffdeac9cfd015b191513b2032b4b2f7d25f2e54
ivancas84 <ivancas84@gmail.com>
2021-03-22 20:20:58 -0300

    Agregar campo calificacion.observaciones

--- src/class/model/entity/_Calificacion.php
+++ src/class/model/entity/_Calificacion.php
@@ -6,7 +6,7 @@ require_once("class/model/Field.php");
 class _CalificacionEntity extends Entity {
   public $name = "calificacion";
   public $alias = "cali";
-  public $nf = ['nota1', 'nota2', 'nota3', 'nota_final', 'crec', 'porcentaje_asistencia'];
+  public $nf = ['nota1', 'nota2', 'nota3', 'nota_final', 'crec', 'porcentaje_asistencia', 'observaciones'];
   public $mu = ['curso', 'persona'];
   public $_u = [];
   public $notNull = ['id', 'curso', 'persona'];
