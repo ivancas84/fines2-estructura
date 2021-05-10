e54c27b9358c491d97da194404d235c2d7f8c76d
ivancas84 <ivancas84@gmail.com>
2021-05-10 10:05:01 -0300

    Incorporar nuevo campo calificacion.division

--- src/class/model/entity/_Calificacion.php
+++ src/class/model/entity/_Calificacion.php
@@ -6,7 +6,7 @@ require_once("class/model/Field.php");
 class _CalificacionEntity extends Entity {
   public $name = "calificacion";
   public $alias = "cali";
-  public $nf = ['nota1', 'nota2', 'nota3', 'nota_final', 'crec', 'porcentaje_asistencia', 'observaciones'];
+  public $nf = ['nota1', 'nota2', 'nota3', 'nota_final', 'crec', 'porcentaje_asistencia', 'observaciones', 'division'];
   public $mu = ['curso', 'persona', 'asignatura', 'planificacion'];
   public $_u = [];
   public $notNull = ['id', 'persona', 'asignatura', 'planificacion'];
