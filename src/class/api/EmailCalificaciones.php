ec989ca989b49b6c72a32b21b82909fe2cd4b8eb
ivancas84 <ivancas84@gmail.com>
2021-07-15 09:06:07 -0300

    Desarrollo de elementos para enviar email a alumnos con las calificaciones

---
+++ src/class/api/EmailCalificaciones.php
@@ -0,0 +1,18 @@
+<?php
+
+
+require_once("class/api/Base.php");
+require_once("function/php_input.php");
+
+class EmailInscripcionApi extends BaseApi {
+
+  public function main() {
+    $data = php_input(); 
+    /**
+     * id:persona.id
+     */
+    $return = $this->container->getController("email_notas")->main($data["id"]);
+    return $return;
+  }
+}
+
