f69b3f94944c8180cbab0425b8602b99f6c3da45
ivancas84 <ivancas84@gmail.com>
2021-11-03 18:41:51 -0300

    Eliminar campos no utilizados de alumno

--- src/class/model/entity/_Alumno.php
+++ src/class/model/entity/_Alumno.php
@@ -6,10 +6,10 @@ require_once("class/model/Field.php");
 class _AlumnoEntity extends Entity {
   public $name = "alumno";
   public $alias = "alum";
-  public $nf = ['anio_ingreso', 'observaciones', 'estado_inscripcion', 'fecha_titulacion', 'anio_inscripcion', 'semestre_inscripcion', 'semestre_ingreso', 'adeuda_legajo', 'adeuda_deudores', 'tiene_dni', 'tiene_partida', 'tiene_cuil', 'tiene_constancia_primaria', 'tiene_certificado_primaria', 'tiene_constancia_secundaria', 'tiene_analitico_secundaria', 'tiene_declaracion_jurada'];
+  public $nf = ['anio_ingreso', 'observaciones', 'estado_inscripcion', 'fecha_titulacion', 'anio_inscripcion', 'semestre_inscripcion', 'semestre_ingreso', 'adeuda_legajo', 'adeuda_deudores'];
   public $mu = ['plan', 'resolucion_inscripcion'];
   public $_u = ['persona'];
-  public $notNull = ['id', 'persona', 'tiene_dni', 'tiene_partida', 'tiene_cuil', 'tiene_constancia_primaria', 'tiene_certificado_primaria', 'tiene_constancia_secundaria', 'tiene_analitico_secundaria', 'tiene_declaracion_jurada'];
+  public $notNull = ['id', 'persona'];
   public $unique = ['id', 'persona'];
 
 
