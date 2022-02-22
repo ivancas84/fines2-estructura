94be56e61a0d650638c427eeca87c4075e2b9ad0
ivancas84 <ivancas84@gmail.com>
2022-02-22 13:59:58 -0300

    Correcciones minimas

--- src/class/controller/ModelTools.php
+++ src/class/controller/ModelTools.php
@@ -142,10 +142,22 @@ AND curso.id IN ('{$idCursos_}')
   }
 
   public function cursoHorario($idCursos){
+    /**
+     * @return [
+     *   "curso" => "id curso",
+     *   "horario" => "Lunes 16:00 a 19:00, Martes 16:00 a 19:00"
+     * ]
+     */
     $idCursos_ = implode("','",$idCursos);
     
     $sql = "
-SELECT curso.id AS curso, GROUP_CONCAT(dia.dia, \" \", TIME_FORMAT(horario.hora_inicio, '%H:%i'), \" a \", TIME_FORMAT(horario.hora_fin, '%H:%i') ORDER BY dia.numero ASC SEPARATOR ', ') AS horario
+SELECT curso.id AS curso, GROUP_CONCAT(
+  dia.dia, 
+  \" \", 
+  TIME_FORMAT(horario.hora_inicio, '%H:%i'), 
+  \" a \", 
+  TIME_FORMAT(horario.hora_fin, '%H:%i') ORDER BY dia.numero ASC SEPARATOR ', '
+) AS horario
 FROM curso
 INNER JOIN horario ON (horario.curso = curso.id)
 INNER JOIN dia ON (dia.id = horario.dia)
