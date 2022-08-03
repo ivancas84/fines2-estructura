4cf1a5e43793563f62a69b6b163e1170c09cd6ed
ivancas84 <ivancas84@gmail.com>
2022-08-03 12:10:24 -0300

    Incorporar nuevo campo calificacion.fecha.

--- src/class/model/entity/_Calificacion.php
+++ src/class/model/entity/_Calificacion.php
@@ -6,7 +6,7 @@ require_once("class/model/Field.php");
 class _CalificacionEntity extends Entity {
   public $name = "calificacion";
   public $alias = "cali";
-  public $nf = ['nota1', 'nota2', 'nota3', 'nota_final', 'crec', 'porcentaje_asistencia', 'observaciones', 'division'];
+  public $nf = ['nota1', 'nota2', 'nota3', 'nota_final', 'crec', 'porcentaje_asistencia', 'observaciones', 'division', 'fecha'];
   public $mu = ['curso', 'alumno', 'disposicion'];
   public $_u = [];
   public $notNull = ['id', 'alumno', 'disposicion'];
