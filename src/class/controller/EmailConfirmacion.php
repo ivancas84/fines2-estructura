f6e1240a89fb766986715b0ef4756accc02261d9
ivancas84 <ivancas84@gmail.com>
2022-08-26 14:04:20 -0300

    Que se reenvie todo a gmail

--- src/class/controller/EmailConfirmacion.php
+++ src/class/controller/EmailConfirmacion.php
@@ -21,7 +21,6 @@ class EmailConfirmacion {
     $mt = $this->container->getController("model_tools");
     $toma = $this->container->getDb()->get("toma",$id);
     $curso = $mt->labelCurso($toma,"cur_");
-    $subject = "Toma de posesión: " . $curso;
 	  $idComision = $toma["cur_comision"];
     
     $horario_ = $this->container->getController("model_tools")->cursoHorario([$toma["curso"]]);
@@ -30,30 +29,33 @@ class EmailConfirmacion {
     $t = $this->container->getRel("toma")->value($toma);
 
     $fechaToma = $t["toma"]->_get("fecha_toma");
-    //$fechaFin = new DateTime('2022-07-27');
+    $fechaFin = new DateTime('2022-12-23');
 
-    $fechaFin = clone $fechaToma;
-    $fechaFin->modify("+ 4 month");
+    // $fechaFin = clone $fechaToma;
+    // $fechaFin->modify("+ 4 month");
   
     $body = '
-<p>Hola ' . $t["docente"]->_get("nombres", "Xx Yy") . ' ' . $t["docente"]->_get("apellidos", "Xx Yy") . ', a continuación se indica el comprobante de toma de posesión.</p>
-<p>Comuníquese en primer término con los referentes, ellos conocen la realidad de cada alumno y ofrecen medios de contacto adicionales para la comunicación.</p>
-<p><a href="https://planfines2.com.ar/docente/referentes-para-docente?comision='.$idComision.'">Contacto Referentes</a></p>
-<p><a href="https://planfines2.com.ar/docente/alumnos-para-docente?comision='.$idComision.'">Lista de Alumnos</a></p>
-<p><a href="https://planfines2.com.ar/wp/planillas-de-finalizacion/">Planillas de finalización e instrucciones</a></p> 
-<p>Contacto de Whatsapp CENS 462: 22167113326</p>
-
-
-
-
-
+<p>Hola ' . $t["docente"]->_get("nombres", "Xx Yy") . ' ' . $t["docente"]->_get("apellidos", "Xx Yy") . ':</p>
+<p>Lea atentamente y verifique los datos del comprobante, serán utilizados para cobrar sus haberes. Si observa algún error, comuníquese inmediatamente para su corrección.</p>
+<p>Conserve este comprobante para futuras referencias.</p>
+<br>
+<p>Se recuerda que el/la docente asume el compromiso de:</p>
+<ul>
+  <li>Asistir a la entrevista con el director para efectivizar la Toma de Posesión</li>
+  <li>Efectuar clases presenciales</li>
+  <li>Mantener al día todas las planillas de finalización (pueden ser solicitadas en cualquier momento)</li>
+  <li>Respetar el formato y las instrucciones de las planillas de finalización (<a href="https://planfines2.com.ar/wp/planillas-de-finalizacion/">Descargar</a>)</li>
+  <li>Avisar inmediatamente cualquier incorporación o baja de alumnos al CENS que no coincidan con la Lista de Alumnos (<a href="https://planfines2.com.ar/docente/alumnos-para-docente?comision='.$idComision.'">Ver</a>)</li>
+  <li>Participar de las mesas examinadoras y reuniones a las que fuera convocado</li> 
+  <li>Comunicar cualquier imprevisto a la coordinación administrativa del CENS</li>
+  <li>Emplear las dos últimas clases del período para recuperación y mesa de exámen</li>
+</ul>
+<p>Comuníquese en primer término con los referentes, ellos conocen la realidad de cada alumno y ofrecen medios de contacto adicionales para la comunicación: <a href="https://planfines2.com.ar/docente/referentes-para-docente?comision='.$idComision.'">Contacto Referentes</a></p>
+<p>Contacto de Whatsapp CENS 462 para cualquier consulta: <a href="https://api.whatsapp.com/send/?phone=54922167113326" target="_blank">22167113326</a></p>
 
 <div style="margin-left:15px;width:600px;padding:20px;border:1px solid #d0d2d2;border-radius:5px;color:#444444">
-      <h4 style="padding:0 0 12px 0;margin:0;font-weight:bold">Toma de posesión: <span>' . $t["docente"]->_get("apellidos","X") . ', ' . $t["docente"]->_get("nombres","Xx Yy") . ' DNI ' . $t["docente"]->_get("numero_documento") . '</span></h4>
+      <h4 style="padding:0 0 12px 0;margin:0;font-weight:bold">Comprobante para Toma de posesión: <span>' . $t["docente"]->_get("apellidos","X") . ', ' . $t["docente"]->_get("nombres","Xx Yy") . ' DNI ' . $t["docente"]->_get("numero_documento") . '</span></h4>
       
-      <div style="clear:both;display:block;margin:0 0 10px 0;padding:10px 0 10px 0;border-top:1px solid #d7d7d7">
-      <p>Por medio de la presente se deja constancia que el docente ha tomado posesión en la asignatura correspondiente al <strong>PLAN DE ESTUDIOS R6321/95-R713/17, del plan FinEs 2 de Terminalidad de Nivel Secundario</strong>, dependiente de la Dirección de Educación de Adultos según se expresa en el siguiente detalle.</p>
-      </div>
       <div style="clear:both;display:block;margin:0 0 10px 0;padding:10px 0 10px 0;border-top:1px solid #d7d7d7">
         <table>
         <tbody>
@@ -89,17 +91,7 @@ class EmailConfirmacion {
         </tbody>
         </table>
       </div>
-      <div style="clear:both;display:block;margin:0 0 10px 0;padding:10px 0 10px 0;border-top:1px solid #d7d7d7">
-        <p>El/la docente asume el compromiso de:</p>
-        <ul>
-          <li>Mentener al día todas las planillas de finalización (pueden ser solicitadas en cualquier momento)</li>
-          <li>Avisar inmediatamente al CENS si hay algun alumno que no se encuentra en la lista</li>
-          <li>Avisar inmediatamente al CENS si hay algun alumno de la lista que no va a cursar y debe ser dado de baja</li>
-          <li>Participar de las mesas examinadoras y reuniones a las que fuera convocado</li> 
-        </ul>
-        <p>Se recuerda que las últimas dos clases corresponden a período de recuperación y mesa de examen</p>
-      </div>    
-    
+   
 </div>
 -- 
 <br>
@@ -110,7 +102,7 @@ Equipo de Coordinadores del Plan Fines 2 CENS 462
 ';
 
 
-    $subject = "Comprobante toma de posesión: " . $curso;
+    $subject = "Comprobante para toma de posesión: " . $curso;
 
     $email =  (!Validation::is_empty($t["docente"]->_get("email_abc"))) ? $t["docente"]->_get("email_abc") : $t["docente"]->_get("email");
 
