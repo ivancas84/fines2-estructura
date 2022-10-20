73f1de1a4f1e2c41601baec290403a23133bc1ba
ivancas84 <ivancas84@gmail.com>
2022-10-20 06:57:22 -0300

    refactorizar a la version 0.5

--- src/class/model/mapping/Comision.php
+++ src/class/model/mapping/Comision.php
@@ -4,24 +4,24 @@ require_once("class/model/entityOptions/Mapping.php");
 class ComisionMapping extends MappingEntityOptions{
 
   public function numero() {
-    return "CONCAT({$this->_pf()}sed.numero, {$this->_pt()}.division, '/', COALESCE({$this->_pf()}pla.anio,''), COALESCE({$this->_pf()}pla.semestre,''))
+    return "CONCAT({$this->_pf()}sede.numero, {$this->_pt()}.division, '/', COALESCE({$this->_pf()}planificacion.anio,''), COALESCE({$this->_pf()}planificacion.semestre,''))
 ";
   }
 
 
   public function tramo() {
-    return "CONCAT(COALESCE({$this->_pf()}pla.anio,''), COALESCE({$this->_pf()}pla.semestre,''))";
+    return "CONCAT(COALESCE({$this->_pf()}planificacion.anio,''), COALESCE({$this->_pf()}planificacion.semestre,''))";
   }
 
   public function label() {
     return $this->numero();
 
     return "CONCAT(
-      {$this->_pf()}sed.numero, {$this->_pt()}.division,
-      IF({$this->_pf()}pla.id, CONCAT({$this->_pf()}pla.anio,{$this->_pf()}pla.semestre), ''),
-      IF({$this->_pf()}cal.id, CONCAT(' ',{$this->_pf()}cal.anio,'-',{$this->_pf()}cal.semestre), ''),
-      IF({$this->_pf()}cal.inicio,CONCAT(' ', {$this->_pf()}cal.inicio),''),
-      IF({$this->_pf()}cal.fin,CONCAT(' ', {$this->_pf()}cal.fin),'')
+      {$this->_pf()}sede.numero, {$this->_pt()}.division,
+      IF({$this->_pf()}planificacion.id, CONCAT({$this->_pf()}planificacion.anio,{$this->_pf()}planificacion.semestre), ''),
+      IF({$this->_pf()}calendario.id, CONCAT(' ',{$this->_pf()}calendario.anio,'-',{$this->_pf()}calendario.semestre), ''),
+      IF({$this->_pf()}calendario.inicio,CONCAT(' ', {$this->_pf()}calendario.inicio),''),
+      IF({$this->_pf()}calendario.fin,CONCAT(' ', {$this->_pf()}calendario.fin),'')
     )";
   }
 
