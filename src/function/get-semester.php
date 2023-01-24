d0d631a725ec42151dd5a114d6f3b89fef45eb78
ivancas84 <ivancas84@gmail.com>
2023-01-24 16:13:26 -0300

    Refactorizar funcion get-semester

--- src/function/get-semester.php
+++ src/function/get-semester.php
@@ -1,5 +1,5 @@
 <?
 function getSemester(DateTime $date = null) {
-    $date = $date || new DateTime();
-    return (intval($date.format("m")) < 6) ? 1 : 2;
+    $date = $date ? $date : new DateTime();
+    return (intval($date->format("m")) < 6) ? 1 : 2;
 }
